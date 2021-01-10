5e4195afe5ed08cff7fd868d325143f69c1164c4
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2021-01-10 15:27:01 +0300

    refactore code

--- main.rb
+++ main.rb
@@ -21,6 +21,14 @@ class Main
     @db
   end
 
+  def get_last_lesson(save_parameter)
+    last_lesson = get_db.execute <<-SQL
+    Select #{save_parameter}_save From users
+    Where login == #{get_user[0]} AND password_hash == #{get_user[1]}
+    SQL
+    last_lesson[0][0]
+  end
+
   def get_user
     @user
   end
@@ -73,7 +81,7 @@ class Main
     @list_widgets = [@learning_button, @testing_button, @setup_button, @support_button]
   end
 
-  public :new_window, :load_main_page, :add_widgets_to_list, :get_db, :get_user
+  public :new_window, :load_main_page, :add_widgets_to_list, :get_db, :get_user, :get_last_lesson
 end
 
 
@@ -129,64 +137,63 @@ TEXT
 end
 
 class SelectionMenu
-  def initialize(root, main, prefix_class_of_selection)
+  def initialize(root, main, working_type)
     @root = root
     main.new_window
     @word_button = TkButton.new(@root) do
       text "Слова"
       font TkFont.new('times 20 bold')
       activebackground "blue"
-      command(proc { Kernel.const_get(prefix_class_of_selection + "Words").new(root, main) })
+      command(proc { LessonSelectionMenu.new(root, main, working_type, "Word") })
       place("relx" => 0.1, "rely" => 0.1, "relwidth" => 0.8, "relheight" => 0.15)
     end
     @hiragana_button = TkButton.new(@root) do
       text "Хирагана"
       font TkFont.new('times 20 bold')
       activebackground "blue"
-      command(proc { Kernel.const_get(prefix_class_of_selection + "Kana").new(root, main, HIRAGANA) })
+      command(proc { LessonSelectionMenu.new(root, main, working_type, "Hiragana") })
       place("relx" => 0.1, "rely" => 0.3, "relwidth" => 0.8, "relheight" => 0.15)
     end
     @katakana_button = TkButton.new(@root) do
       text "Катакана"
       font TkFont.new('times 20 bold')
       activebackground "blue"
-      command(proc { Kernel.const_get(prefix_class_of_selection + "Kana").new(root, main, KATAKANA) })
+      command(proc { LessonSelectionMenu.new(root, main, working_type, "Katakana") })
       place("relx" => 0.1, "rely" => 0.5, "relwidth" => 0.8, "relheight" => 0.15)
     end
     @kanji_button = TkButton.new(@root) do
       text "Кандзи"
       font TkFont.new('times 20 bold')
       activebackground "blue"
-      command(proc { Kernel.const_get(prefix_class_of_selection + "Kanji").new(root, main) })
+      command(proc { LessonSelectionMenu.new(root, main, working_type, "Kanji") })
       place("relx" => 0.1, "rely" => 0.7, "relwidth" => 0.8, "relheight" => 0.15)
     end
     main.add_widgets_to_list([@word_button, @hiragana_button, @kanji_button, @katakana_button])
   end
 end
 
-class LearnKana
-  def initialize(root, main, type_of_kana)
+class LessonSelectionMenu
+  def initialize(root, main, working_type, element_type)
+    # working_type can be 1) "Learn" 2) "Test"
+    unless %w[Learn Test].include?(working_type)
+      raise "Check working_type!!, not #{element_type}"
+    end
+    # element_type can be 1) "Word" 2) "Kanji" 3) "Hiragana" 4) "Katakana"
+    unless %w[Word Hiragana Katakana Kanji].include?(element_type)
+      raise("Check element_type!!, not #{element_type}")
+    end
     @root = root
     main.new_window
-    kana_table = (
-    if type_of_kana == HIRAGANA then
-      "hiragana"
-    else
-      "katakana"
-    end)
-    kana = main.get_db.execute <<-SQL
-    Select * From #{kana_table}
+    elements = main.get_db.execute <<-SQL
+    Select * From #{element_type.downcase}
     SQL
-    last_lesson = main.get_db.execute <<-SQL
-    Select #{type_of_kana}_save From users
-    Where login == #{main.get_user[0]} AND password_hash == #{main.get_user[1]}
-    SQL
-    num_of_lessons = (kana.length / COUNT_OF_LEARNING).ceil
+    last_lesson = main.get_last_lesson(element_type.downcase)
+    num_of_lessons = (elements.length / COUNT_OF_LEARNING).ceil
     list = TkListbox.new(@root) do
       place("relx" => 0.1, "rely" => 0.1, "relwidth" => 0.6, "relheight" => 0.7)
     end
     (1..num_of_lessons).to_a.each do |lesson|
-      if lesson <= last_lesson[0][0]
+      if lesson <= last_lesson
         list.insert(lesson - 1, lesson)
       else
         list.insert(lesson - 1, lesson.to_s + " |Ещё не открыт!|")
@@ -214,129 +221,15 @@ class LearnKana
       font TkFont.new('times 20 bold')
       activebackground "blue"
       command(proc {
-        LearnKanaElement.new(@root, main, (
-        if list.curselection[0] then
-          list.curselection[0]
-        else
-          0
+        if list.curselection[0] + 1 <= last_lesson or not list.curselection[0]
+          Kernel.const_get(working_type + element_type + "Element").new(@root, main, (
+          if list.curselection[0]
+            list.curselection[0]
+          else
+            0
+          end
+          ), 1)
         end
-        ), 1, type_of_kana)
-      })
-      place("relx" => 0.1, "rely" => 0.85, "relwidth" => 0.8, "relheight" => 0.1)
-    end
-    main.add_widgets_to_list([list, @menu_button, @scroll, @confirm_button])
-  end
-end
-
-class LearnKanji
-  def initialize(root, main)
-    @root = root
-    main.new_window
-    kanji = main.get_db.execute <<-SQL
-    Select * From kanji
-    SQL
-    last_lesson = main.get_db.execute <<-SQL
-    Select kanji_save From users
-    Where login == #{main.get_user[0]} AND password_hash == #{main.get_user[1]}
-    SQL
-    num_of_lessons = (kanji.length / COUNT_OF_LEARNING).ceil
-    list = TkListbox.new(@root) do
-      place("relx" => 0.1, "rely" => 0.1, "relwidth" => 0.6, "relheight" => 0.7)
-    end
-    (1..num_of_lessons).to_a.each do |lesson|
-      if lesson <= last_lesson[0][0]
-        list.insert(lesson - 1, lesson)
-      else
-        list.insert(lesson - 1, lesson.to_s + " |Ещё не открыт!|")
-      end
-    end
-    @scroll = TkScrollbar.new(@root) do
-      orient 'vertical'
-      place("relx" => 0.7, "rely" => 0.1, "relwidth" => 0.015, "relheight" => 0.7)
-    end
-    list.yscrollcommand(proc { |*args|
-      @scroll.set(*args)
-    })
-    @scroll.command(proc { |*args|
-      list.yview(*args)
-    })
-    @menu_button = TkButton.new(@root) do
-      text "Меню"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc { main.load_main_page })
-      place("relx" => 0.8, "rely" => 0.1, "relwidth" => 0.1, "relheight" => 0.1)
-    end
-    @confirm_button = TkButton.new(@root) do
-      text "Выбрать"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc {
-        LearnKanjiElement.new(@root, main, (
-        if list.curselection[0] then
-          list.curselection[0]
-        else
-          0
-        end
-        ), 1)
-      })
-      place("relx" => 0.1, "rely" => 0.85, "relwidth" => 0.8, "relheight" => 0.1)
-    end
-    main.add_widgets_to_list([list, @menu_button, @scroll, @confirm_button])
-  end
-end
-
-class LearnWords
-  def initialize(root, main)
-    @root = root
-    main.new_window
-    words = main.get_db.execute <<-SQL
-    Select * From word
-    SQL
-    last_lesson = main.get_db.execute <<-SQL
-    Select words_save From users
-    Where login == #{main.get_user[0]} AND password_hash == #{main.get_user[1]}
-    SQL
-    num_of_lessons = (words.length / COUNT_OF_LEARNING).ceil
-    list = TkListbox.new(@root) do
-      place("relx" => 0.1, "rely" => 0.1, "relwidth" => 0.6, "relheight" => 0.7)
-    end
-    (1..num_of_lessons).to_a.each do |lesson|
-      if lesson <= last_lesson[0][0]
-        list.insert(lesson - 1, lesson)
-      else
-        list.insert(lesson - 1, lesson.to_s + " |Ещё не открыт!|")
-      end
-    end
-    @scroll = TkScrollbar.new(@root) do
-      orient 'vertical'
-      place("relx" => 0.7, "rely" => 0.1, "relwidth" => 0.015, "relheight" => 0.7)
-    end
-    list.yscrollcommand(proc { |*args|
-      @scroll.set(*args)
-    })
-    @scroll.command(proc { |*args|
-      list.yview(*args)
-    })
-    @menu_button = TkButton.new(@root) do
-      text "Меню"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc { main.load_main_page })
-      place("relx" => 0.8, "rely" => 0.1, "relwidth" => 0.1, "relheight" => 0.1)
-    end
-    @confirm_button = TkButton.new(@root) do
-      text "Выбрать"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc {
-        LearnWordElement.new(@root, main, (
-        if list.curselection[0] then
-          list.curselection[0]
-        else
-          0
-        end
-        ), 1)
       })
       place("relx" => 0.1, "rely" => 0.85, "relwidth" => 0.8, "relheight" => 0.1)
     end
@@ -351,7 +244,7 @@ class LearnKanaElement
     start_id = COUNT_OF_LEARNING * lesson + 1
     end_id = start_id + COUNT_OF_LEARNING - 1
     kana_table = (
-    if type_of_kana == HIRAGANA then
+    if type_of_kana == HIRAGANA
       "hiragana"
     else
       "katakana"
@@ -448,7 +341,7 @@ class LearnKanjiElement
       command(proc { main.load_main_page })
       place("relx" => 0.8, "rely" => 0.1, "relwidth" => 0.1, "relheight" => 0.1)
     end
-    widgets = [@writing_text, @onyomi_reading_text, @kunyomi_reading_text, @meaning_text, @menu_button]
+    widgets = [@writing_text, @onyomi_reading_text, @kunyomi_reading_text, @meaning_text, @examples_text, @menu_button]
     unless current_word[7].is_a?(NilClass)
       @image_kanji = TkPhotoImage.new
       @image_kanji.file = ".//" + current_word[7]
@@ -539,61 +432,20 @@ class LearnWordElement
   end
 end
 
-class TestWords
-  def initialize(root, main)
+class TestWordElement
+  def initialize(root, main, lesson, index)
     @root = root
     main.new_window
+    start_id = COUNT_OF_LEARNING * lesson + 1
+    end_id = start_id + COUNT_OF_LEARNING - 1
     words = main.get_db.execute <<-SQL
-    Select * From word
+      Select * From word
+        Where id >= #{start_id} AND id <= #{end_id}
     SQL
-    last_lesson = main.get_db.execute <<-SQL
-    Select words_save From users
-    Where login == #{main.get_user[0]} AND password_hash == #{main.get_user[1]}
-    SQL
-    num_of_lessons = (words.length / COUNT_OF_LEARNING).ceil
-    list = TkListbox.new(@root) do
-      place("relx" => 0.1, "rely" => 0.1, "relwidth" => 0.6, "relheight" => 0.7)
-    end
-    (1..num_of_lessons).to_a.each do |lesson|
-      if lesson <= last_lesson[0][0]
-        list.insert(lesson - 1, lesson)
-      else
-        list.insert(lesson - 1, lesson.to_s + " |Ещё не открыт!|")
-      end
-    end
-    @scroll = TkScrollbar.new(@root) do
-      orient 'vertical'
-      place("relx" => 0.7, "rely" => 0.1, "relwidth" => 0.015, "relheight" => 0.7)
-    end
-    list.yscrollcommand(proc { |*args|
-      @scroll.set(*args)
-    })
-    @scroll.command(proc { |*args|
-      list.yview(*args)
-    })
-    @menu_button = TkButton.new(@root) do
-      text "Меню"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc { main.load_main_page })
-      place("relx" => 0.8, "rely" => 0.1, "relwidth" => 0.1, "relheight" => 0.1)
-    end
-    @confirm_button = TkButton.new(@root) do
-      text "Выбрать"
-      font TkFont.new('times 20 bold')
-      activebackground "blue"
-      command(proc { 
-        TestWordElement.new(@root, main, (
-        if list.curselection[0] then
-          list.curselection[0]
-        else
-          0
-        end
-        ), 1)
-      })
-      place("relx" => 0.1, "rely" => 0.85, "relwidth" => 0.8, "relheight" => 0.1)
-    end
-    main.add_widgets_to_list([list, @menu_button, @scroll, @confirm_button])
+    current_word = words[index - 1]
+
+    widgets = []
+    main.add_widgets_to_list(widgets)
   end
 end
 
